d674b7a587eb41a8d38c1bce0506bf023cc950fc
ArslanBaris <arslan.bariss@outlook.com>
2023-10-20 21:09:04 +0300

    Navbar created

---
+++ components/Layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Navbar() {
+
+
+
+
+  return (
+    <>
+         <div className="navbar text-center w-100 d-flex justify-content-center" style={{alignItems:"center"}}>
+            <h1 className="navbar-title">Logo</h1>
+        </div>
+    </>
+  );
+}
