2d97b69f859470cd73fab576e6592560da9b8adc
ArslanBaris <arslan.bariss@outlook.com>
2023-10-20 21:12:41 +0300

    mockup data added

---
+++ components/data.js
@@ -0,0 +1,103 @@
+const categories = 
+{
+  "categories": [
+    {
+      "category_id": 1,
+      "name": "Sevgiliye Özel",
+      "questions": [
+        {
+          "question_id": 1,
+          "content": "Sevgilinizin en sevdiği mevsim nedir?",
+          "answers": [
+            {"answer_id": 1, "content": "Kış"},
+            {"answer_id": 2, "content": "İlkbahar"},
+            {"answer_id": 3, "content": "Yaz"},
+            {"answer_id": 4, "content": "Sonbahar"}
+          ]
+        },
+        {
+          "question_id": 2,
+          "content": "Sevgiliniz için ideal bir tatil neresi olurdu?",
+          "answers": [
+            {"answer_id": 5, "content": "Tropikal bir ada"},
+            {"answer_id": 6, "content": "Büyüleyici bir Avrupa şehri"},
+            {"answer_id": 7, "content": "Yıldızların altında kamp yapmak"},
+            {"answer_id": 8, "content": "Sessiz bir göl kenarı tatili"}
+          ]
+        },
+        {
+          "question_id": 3,
+          "content": "Sevgilinizin en sevdiği yemek türü nedir?",
+          "answers": [
+            {"answer_id": 9, "content": "Akdeniz mutfağı"},
+            {"answer_id": 10, "content": "Uzakdoğu yemekleri"},
+            {"answer_id": 11, "content": "Fast food ve atıştırmalıklar"},
+            {"answer_id": 12, "content": "Vejetaryen veya vegan yemekler"},
+            {"answer_id": 13, "content": "Tatlı ve pastalar"}
+          ]
+        },
+        {
+          "question_id": 4,
+          "content": "Sevgilinizin hoşlandığı bir aktivite seçin?",
+          "answers": [
+            {"answer_id": 14, "content": "Dans etmek"},
+            {"answer_id": 15, "content": "Yürüyüşe çıkmak"},
+            {"answer_id": 16, "content": "Müze veya sergi ziyareti"},
+            {"answer_id": 17, "content": "Evcil hayvanlarla vakit geçirmek"},
+            {"answer_id": 18, "content": "Spor yapmak"}
+          ]
+        }
+      ]
+    },
+{
+      "category_id": 2,
+      "name": "Doğum Günü",
+      "questions": [
+        {
+          "question_id": 5,
+          "content": "Doğum günü kişisinin kişiliği nasıl tanımlanabilir?",
+          "answers": [
+            {"answer_id": 19, "content": "Romantik ve hassas"},
+            {"answer_id": 20, "content": "Macera sever ve enerjik"},
+            {"answer_id": 21, "content": "Sadelikten hoşlanır"},
+            {"answer_id": 22, "content": "Sanatsal ve yaratıcı"},
+            {"answer_id": 23, "content": "Sporcu ve aktif"}
+          ]
+        },
+        {
+          "question_id": 6,
+          "content": "Doğum günü kişisi hangi renkleri tercih eder?",
+          "answers": [
+            {"answer_id": 24, "content": "Mavi"},
+            {"answer_id": 25, "content": "Pembe"},
+            {"answer_id": 26, "content": "Yeşil"},
+            {"answer_id": 27, "content": "Kırmızı"},
+            {"answer_id": 28, "content": "Sarı"}
+          ]
+        },
+        {
+          "question_id": 7,
+          "content": "Doğum günü kişisi için önemli olan bir hedef veya hayali nedir?",
+          "answers": [
+            {"answer_id": 29, "content": "Yeni yerler keşfetmek"},
+            {"answer_id": 30, "content": "Sağlıklı yaşam tarzını sürdürmek"},
+            {"answer_id": 31, "content": "Kariyerinde ilerlemek"},
+            {"answer_id": 32, "content": "Kendine daha fazla zaman ayırmak"},
+            {"answer_id": 33, "content": "Finansal özgürlüğe ulaşmak"}
+          ]
+        },
+        {
+          "question_id": 8,
+          "content": "Doğum günü kişisi en son nerede seyahat etti?",
+          "answers": [
+            {"answer_id": 34, "content": "Deniz kenarı bir tatil beldesi"},
+            {"answer_id": 35, "content": "Tarihi bir şehir veya kasaba"},
+            {"answer_id": 36, "content": "Dağlar veya doğa parkları"},
+            {"answer_id": 37, "content": "Yurtdışı seyahati"},
+            {"answer_id": 38, "content": "Macera dolu bir yolculuk"}
+          ]
+        }
+      ]
+    }
+  ]
+}
